8f76421b1aceb5a992b322a7617a956414a001f0
jschust2 <jschust2>
2016-01-03 13:48:49 -0500

    From GUI again, unable to get Atom to GIT, problem with GIT not being a windows command

--- script.js
+++ script.js
@@ -6,14 +6,22 @@
 // Events - Things that are triggered as the user does
 //          sactions  on the page, we listen for them
 
-// The event her is 'load', when the page is loaded
+// The event her is 'load', when the entire page is loaded
 
 window.addEventListener('load', function() {
     console.log('the page has been loaded!');
 });
 
-console.log("poop");
+// Listen for the "click" event
+//   This is just a verification it's working
+function searchIt(){
+  console.log("I'm searching already!");
+}
+//    Get the Button
+var btnSearch = document.getElementById('searchButton');
+//    Listen to the button, when it's clicked,
+//  run the searchIt function
+btnSearch.addEventListener('click', searchIt);
 
-function btnClicked (){
-  console.log('Button clicked');
-};
+
+console.log("poop");
