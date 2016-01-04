4c28e8cfd234c1b07af7c5350b916b83d97a2dbf
john.schuster@phoenixworkgroup.com <john.schuster@phoenixworkgroup.com>
2016-01-03 20:47:48 -0500

    Stupid having 2 script.js files, was using one in root no JS directory

--- js/script.js
+++ js/script.js
@@ -6,14 +6,40 @@
 // Events - Things that are triggered as the user does
 //          sactions  on the page, we listen for them
 
-// The event her is 'load', when the page is loaded
 
-window.addEventListener('load', function() {
-    console.log('the page has been loaded!');
-});
+// Alternative method of event listerner
+// document.getElementById('searchButton').addEventListener("click", searchIt,false);
 
-console.log("poop");
 
-function btnClicked (){
-  console.log('Button clicked');
-};
+// The event here is 'load', when the entire page is loaded
+// window.addEventListener('load', function() {
+//     console.log('the page has been loaded!');
+    //document.getElementById('searchButton').addEventListener("click", searchIt,false);
+// });
+
+// Listen for the "click" event
+//   This is just a verification it's working
+function searchIt(){
+  console.log("I'm searching already!");
+}
+
+function hoverIt() {
+  console.log("Someone is over me");
+}
+//    Get the Button
+var btnSearch = document.getElementById('searchButton');
+//    Listen to the button, when it's clicked,
+//  run the searchIt function
+//
+// Having some issues with this working..  ss
+btnSearch.addEventListener('click', searchIt);
+btnSearch.addEventListener('mouseover', hoverIt);
+
+
+
+
+
+// Notice in Console that this is NOT the last log message
+// The window event Listener at the top generartaes the
+// Last message
+console.log("pooooop");
